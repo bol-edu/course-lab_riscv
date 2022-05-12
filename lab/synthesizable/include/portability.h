ffe862808b88eac272081cdfa00d41d11535fdd2
Leng-Kai <45026969+Leng-Kai@users.noreply.github.com>
2022-05-12 14:13:11 +0800

    remove unused header

--- lab/synthesizable/include/portability.h
+++ lab/synthesizable/include/portability.h
@@ -7,16 +7,4 @@
 #define SLC(size, low) range(size + low - 1, low)
 #define SET_SLC(low, value) range(low + value.length() - 1, low) = value
 
-#include <hls_stream.h>
-#include <ap_shift_reg.h>
-#define STREAM(param) hls::stream<param>
-#define SREAD() read()
-#define SWRITE(value) write(value)
-
-// #define STREAM(param) ap_shift_reg<param, 1>
-// #define SREAD() read(0)
-// #define SWRITE(value) shift(value, 0)
-
-// #define STREAM(param) param
-
 #endif /* For PORTABILITY_H_ */
